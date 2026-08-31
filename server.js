d2241bc26232483d7f3bc5c959a7de19ddc93397
ville <villenalascanoadamarieliana@gmail.com>
2026-08-30 23:38:34 -0400

    Preparar servidor para despliegue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const databasePath = path.join(
     __dirname,
